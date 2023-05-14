f24b2787b27b73aa3389502a8472e7f6807b7fe5
Oscar Marin <oscar@marin.cr>
2023-05-14 11:16:29 -0500

    added information about the max number of tokens per model

--- code/Luval.OpenAI/Model/ModelResponse.cs
+++ code/Luval.OpenAI/Model/ModelResponse.cs
@@ -143,9 +143,30 @@ namespace Luval.OpenAI.Models
         [JsonProperty("parent")]
         public object Parent { get; set; }
 
+
         #endregion
 
 
+    }
+
+    public static class ModelMaxTokens
+    {
+        public static Dictionary<string, int> Instance => new Dictionary<string, int>()
+                                                            {
+                                                                {"gpt-4", 8192},
+                                                                {"gpt-4-0314", 8192},
+                                                                {"gpt-4-32k", 32768},
+                                                                {"gpt-4-32k-0314", 32768},
+                                                                {"gpt-3.5-turbo", 4096},
+                                                                {"gpt-3.5-turbo-0301", 4096},
+                                                                {"text-davinci-003", 4097},
+                                                                {"text-davinci-002", 4097},
+                                                                {"code-davinci-002", 8001},
+                                                                {"davinci", 2048},
+                                                                {"curie", 2048},
+                                                                {"babbage", 4096},
+                                                                {"ada", 4096},
+                                                            };
     }
 
 }
